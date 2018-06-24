5f5e8b3b467895b55fddb76bc377ea1038c0fff3
Bartosz Pawlicki <bartekp44@gmail.com>
2018-06-24 18:48:49 +0200

    Create directories. Create player with movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public float speedFactor;
+
+    private float moveHorizontal = 0;
+    private float moveVertical = 0;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+
+        if (moveHorizontal != 0 || moveVertical != 0)
+        {
+            Vector3 movement = transform.position + new Vector3(moveHorizontal, 0, moveVertical) * speedFactor;
+            rigidbody.MovePosition(movement);
+        }
+
+
+    }
+}
