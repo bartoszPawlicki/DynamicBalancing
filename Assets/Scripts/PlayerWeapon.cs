821b95542543a54fcf546792056b0bfe2a47347c
Bartosz Pawlicki <bartekp44@gmail.com>
2018-10-08 15:29:57 +0200

    Add rocket weapon

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class PlayerWeapon : MonoBehaviour
 {
     public ObjectPool bulletPool;
+    public ObjectPool rocketPool;
     public Cooldown cooldown;
+    public Cooldown rocketCooldown;
 
     float shootHorizontal = 0;
     float shootVertical = 0;
+    float lastShootHorizontal = 0;
+    float lastShootVertical = 0;
 
     // Use this for initialization
     void Start ()
     {
         cooldown.InitCooldown();
+        rocketCooldown.InitCooldown();
     }
     
     void Update()
@@ -27,18 +32,38 @@ public class PlayerWeapon : MonoBehaviour
 
                 if (shootHorizontal != 0)
                 {
+                    lastShootHorizontal = shootHorizontal;
                     Shoot(new Vector3(shootHorizontal, 0, 0));
                 }
 
                 else if (shootVertical != 0)
                 {
+                    lastShootVertical = shootVertical;
                     Shoot(new Vector3(0, 0, shootVertical));
                 }
             }
-        }
-        
-        
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (rocketCooldown.canUse)
+                {
+                    shootHorizontal = lastShootHorizontal;
+                    shootVertical = lastShootVertical;
 
+                    if (shootHorizontal != 0)
+                    {
+                        ShootRocket(new Vector3(shootHorizontal, 0, 0));
+                    }
+
+                    else if (shootVertical != 0)
+                    {
+                        ShootRocket(new Vector3(0, 0, shootVertical));
+                    }
+                }
+            }
+            
+
+
+        }
     }
 
     void Shoot(Vector3 direction)
@@ -66,5 +91,32 @@ public class PlayerWeapon : MonoBehaviour
         GameObject bullet = bulletPool.PoolNext(transform.position + translationVec);
 
         bullet.GetComponent<Bullet>().StartBulletMovement(direction);
+    }
+
+    void ShootRocket(Vector3 direction)
+    {
+        rocketCooldown.startTimer();
+
+        Vector3 translationVec = Vector3.zero;
+
+        if (direction.x < 0)
+        {
+            translationVec = Vector3.left;
+        }
+        else if (direction.x > 0)
+        {
+            translationVec = Vector3.right;
+        }
+        else if (direction.z < 0)
+        {
+            translationVec = Vector3.back;
+        }
+        else if (direction.z > 0)
+        {
+            translationVec = Vector3.forward;
+        }
+        GameObject rocket = rocketPool.PoolNext(transform.position + translationVec * 3 + Vector3.up * 8);
+        rocket.GetComponent<Rocket>().StartRocketMovement(Vector3.down);
+
     }
 }
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    public float bulletSpeed;
+    public GameObject player;
+    public UberCanvasScript canvas;
+    private void OnEnable()
+    {
+
+    }
+
+    public void StartRocketMovement(Vector3 direction)
+    {
+
+        Vector3 dir = direction.normalized;
+        GetComponent<Rigidbody>().AddForce(dir.normalized * bulletSpeed);
+        StartCoroutine(DestroyRocket());
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<EnemyController>().ReceiveDamage(2);
+            gameObject.SetActive(false);
+        }
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+
+    IEnumerator DestroyRocket()
+    {
+        yield return new WaitForSeconds(3f);
+        gameObject.SetActive(false);
+    }
+}
